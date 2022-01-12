f5b7b06150272df0e2b409c2ce9e712ac8088c48
nerdstarcode <sthivendev@gmail.com>
2022-01-12 01:15:57 -0300

    utilização do require e module.exports

---
+++ calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./funcoes/somar");
+var SubFunc = require("./funcoes/subtrair");
+var MultiFunc = require("./funcoes/multiplicar");
+var DivFunc = require("./funcoes/divisao");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultiFunc(100,10));
+console.log(DivFunc(20,10));
